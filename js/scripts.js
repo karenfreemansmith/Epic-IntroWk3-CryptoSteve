db27e90baffc333305dc5a111f1c04c81b0e6afc
Ewa Manek, Jena SanCartier and Karen Freeman-Smith <jena@>
2016-08-18 15:13:04 -0700

    properly place letters in grid

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,22 @@ function encode(phrase) {
   var cols=0;
   phrase = phrase.toLowerCase();
   phrase = phrase.replace(/[^a-z]/g,'');
-  cols = Math.sqrt(phrase.length);
-  alert(cols);
+  cols = parseInt(Math.sqrt(phrase.length));
+  var bigArray = [];
+  var smallArray = [];
+  var counter = 0;
+  for(var i = 0; i <= phrase.length; i++){
+    //debugger;
+    if(counter < cols){
+      smallArray.push(phrase.charAt(i));
+      counter++;
+    } else {
+      bigArray.push(smallArray);
+      counter = 0;
+      smallArray = [];
+      i--;
+    }
+  }
+  console.log(bigArray);
   return phrase;
 }
